4a2011e0badeb1b07859c5f9ade5ab4bd67f5fee
Tilman Hoffbauer <tilman.hoffbauer@rwth-aachen.de>
2021-01-14 18:15:34 +0100

    Streamline WMD and ICT implementation

--- Code/conformance_checking/distances.py
+++ Code/conformance_checking/distances.py
@@ -1,10 +1,11 @@
 from pyemd import emd
 import numpy as np
-import copy
-import collections
 from typing import Dict, Tuple
 
-def calc_wmd(model_embedding: Dict[Tuple, int], real_embedding: Dict[Tuple, int]) -> float:
+
+def calc_wmd(
+    model_embedding: Dict[Tuple, int], real_embedding: Dict[Tuple, int]
+) -> float:
     """calculates the dissimilarity between two embeddings.
 
     :param model_embedding: the embedding of the model trace
@@ -14,8 +15,9 @@ def calc_wmd(model_embedding: Dict[Tuple, int], real_embedding: Dict[Tuple, int]
     """
 
     whole_embedding = set(list(model_embedding.keys()) + list(real_embedding.keys()))
-    
+
     vocab_len = len(whole_embedding)
+
     # function: calculate normalized count of activity i within its trace
     def calc_d(embeddings: dict):
         d = np.zeros(vocab_len, dtype=np.double)
@@ -23,7 +25,7 @@ def calc_wmd(model_embedding: Dict[Tuple, int], real_embedding: Dict[Tuple, int]
         trace_len = 0
         for value in embeddings.values():
             trace_len += value
-            
+
         for i, embedding in enumerate(whole_embedding):
             count = embeddings.get(embedding, 0)
             d[i] = count / trace_len
@@ -31,45 +33,55 @@ def calc_wmd(model_embedding: Dict[Tuple, int], real_embedding: Dict[Tuple, int]
 
     d_model = calc_d(model_embedding)
     d_real = calc_d(real_embedding)
-    
+
     # calculate Euclidean distance between embeddings word i and word j
     distance_matrix = np.zeros((vocab_len, vocab_len), dtype=np.double)
     for i, embedding1 in enumerate(whole_embedding):
         for j, embedding2 in enumerate(whole_embedding):
-            if distance_matrix[i, j] != 0.0: continue
-            distance_matrix[i, j] = distance_matrix[j, i] = \
-                np.sqrt(np.sum((np.array(embedding1) - np.array(embedding2))**2))
+            if distance_matrix[i, j] != 0.0:
+                continue
+            distance_matrix[i, j] = distance_matrix[j, i] = np.sqrt(
+                np.sum((np.array(embedding1) - np.array(embedding2)) ** 2)
+            )
 
     dist = emd(d_model, d_real, distance_matrix)
 
     return dist
 
-def ACT(p, q, C, k): #for now C is new every trace comparison, ADD LATER old used for the early stopping
+
+def ACT(
+    p, q, C, k
+):  # for now C is new every trace comparison, ADD LATER old used for the early stopping
     t = 0
     for i in range(0, len(p)):
-        pi = p[i] #the weight of the ith element in p trace
-        if pi == 0.: #if this activity is not actually in p pi will be zero
+        pi = p[i]  # the weight of the ith element in p trace
+        if pi == 0.0:  # if this activity is not actually in p pi will be zero
             continue
-        dummy_s = np.argsort(C[i]) #have to change to only use the thing where q[j] != 0
+        dummy_s = np.argsort(
+            C[i]
+        )  # have to change to only use the thing where q[j] != 0
         s = np.ones(k, dtype=int)
         it = 0
         j = 0
         while it < k and j < len(dummy_s):
-            if q[dummy_s[j]] != 0.:
+            if q[dummy_s[j]] != 0.0:
                 s[it] = int(dummy_s[j])
                 it = it + 1
             j = j + 1
-        l = 0
+        l = 0  # noqa: E741
         while l < k and pi > 0:
             r = min(pi, q[s[l]])
             pi = pi - r
-            t = t + r * C[i, s[l]] 
-            l = l + 1
+            t = t + r * C[i, s[l]]
+            l = l + 1  # noqa: E741
         if pi != 0:
-            t =  t + pi * C[i, s[l - 1]]
+            t = t + pi * C[i, s[l - 1]]
     return t
 
-def calc_ict(model_embedding: Dict[Tuple, int], real_embedding: Dict[Tuple, int], k: int)  -> float:
+
+def calc_ict(
+    model_embedding: Dict[Tuple, int], real_embedding: Dict[Tuple, int], k: int = 3
+) -> float:
     """calculates the dissimilarity between two embeddings.
 
     :param model_embedding: the embedding of the model trace
@@ -81,6 +93,7 @@ def calc_ict(model_embedding: Dict[Tuple, int], real_embedding: Dict[Tuple, int]
     whole_embedding = set(list(model_embedding.keys()) + list(real_embedding.keys()))
 
     vocab_len = len(whole_embedding)
+
     # function: calculate normalized count of activity i within its trace
     def calc_d(embeddings: dict):
         d = np.zeros(vocab_len, dtype=np.double)
@@ -88,7 +101,7 @@ def calc_ict(model_embedding: Dict[Tuple, int], real_embedding: Dict[Tuple, int]
         trace_len = 0
         for value in embeddings.values():
             trace_len += value
-            
+
         for i, embedding in enumerate(whole_embedding):
             count = embeddings.get(embedding, 0)
             d[i] = count / trace_len
@@ -96,14 +109,16 @@ def calc_ict(model_embedding: Dict[Tuple, int], real_embedding: Dict[Tuple, int]
 
     d_model = calc_d(model_embedding)
     d_real = calc_d(real_embedding)
-    
+
     # calculate Euclidean distance between embeddings word i and word j
     distance_matrix = np.zeros((vocab_len, vocab_len), dtype=np.double)
     for i, embedding1 in enumerate(whole_embedding):
         for j, embedding2 in enumerate(whole_embedding):
-            if distance_matrix[i, j] != 0.0: continue
-            distance_matrix[i, j] = distance_matrix[j, i] = \
-                np.sqrt(np.sum((np.array(embedding1) - np.array(embedding2))**2))
+            if distance_matrix[i, j] != 0.0:
+                continue
+            distance_matrix[i, j] = distance_matrix[j, i] = np.sqrt(
+                np.sum((np.array(embedding1) - np.array(embedding2)) ** 2)
+            )
 
     dist = ACT(d_model, d_real, distance_matrix, k)
 
---
+++ Code/tests/test_distances.py
@@ -0,0 +1,17 @@
+import pytest
+
+from conformance_checking.distances import calc_wmd, calc_ict
+
+
+def test_wmd():
+    """Is the wmd calculated correctly?"""
+    model_embedding = {(1, 4): 1, (5, 1): 1}
+    real_embedding = {(5, 1): 1}
+    assert calc_wmd(model_embedding, real_embedding) == pytest.approx(2.5)
+
+
+def test_ict():
+    """Is the ict calculated correctly?"""
+    model_embedding = {(1, 4): 1, (5, 1): 1}
+    real_embedding = {(5, 1): 1}
+    assert calc_ict(model_embedding, real_embedding) == pytest.approx(2.5)
