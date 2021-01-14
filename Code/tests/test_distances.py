5461e5fc9f54385e317dc0ffd22c1620717f3463
Tilman Hoffbauer <tilman.hoffbauer@rwth-aachen.de>
2021-01-14 18:21:06 +0100

    Add a second ICT test to improve coverage

--- Code/tests/test_distances.py
+++ Code/tests/test_distances.py
@@ -15,3 +15,10 @@ def test_ict():
     model_embedding = {(1, 4): 1, (5, 1): 1}
     real_embedding = {(5, 1): 1}
     assert calc_ict(model_embedding, real_embedding) == pytest.approx(2.5)
+
+
+def test_ict2():
+    """Is the ict calculated correctly?"""
+    model_embedding = {(0, 4): 1}
+    real_embedding = {(500, 4): 1}
+    assert calc_ict(model_embedding, real_embedding) == pytest.approx(500)
