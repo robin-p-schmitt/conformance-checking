13c07a4d44e5f44ce1b5f6473fc9f50f3659c750
Chenyang Li <li_chenyang@hotmail.com>
2021-01-15 19:00:40 +0100

    Update after running flake8

--- Code/conformance_checking/wmd_ict.py
+++ Code/conformance_checking/wmd_ict.py
@@ -1,16 +1,17 @@
 from conformance_checking.distances import calc_wmd, calc_ict
 
+
 def main():
     # create some embeddings as example
-    # (int, int, ...): int = embedding of a activity: count of this activity within a trace
-    model_embedding = {(0.4, 0.3): 3, 
-                       (0.2, 0.6): 1,
-                       (0.5, 0.9): 2}
+    # (int, int, ...): int =
+    # embedding of a activity: count of this activity within a trace
+    model_embedding = {(0.4, 0.3): 3, (0.2, 0.6): 1, (0.5, 0.9): 2}
     real_embedding = {(0.4, 0.3): 2}
 
     # calculate WMD between these two traces
     print("WMD: ", calc_wmd(model_embedding, real_embedding))
     print("ICT: ", calc_ict(model_embedding, real_embedding))
+
 
 if __name__ == "__main__":
     main()
