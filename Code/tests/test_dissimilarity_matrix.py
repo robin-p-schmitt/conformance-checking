6f7dc9cebe9427e97aec1dc4e44fbf46ffc91461
Tilman Hoffbauer <tilman.hoffbauer@rwth-aachen.de>
2021-01-24 13:39:27 +0100

    Fix formatting

--- Code/tests/test_dissimilarity_matrix.py
+++ Code/tests/test_dissimilarity_matrix.py
@@ -5,47 +5,63 @@ import conformance_checking as cf
 
 
 def test_calc_fitness_precision_1():
-    matrix = cf.DissimilarityMatrix(np.asarray([
-        # dissimilarities are maximal for all model/real trace pairs
-        # thus, precision and fitness is minimal
-        [1.0, 1.0],
-        [1.0, 1.0],
-    ]))
+    matrix = cf.DissimilarityMatrix(
+        np.asarray(
+            [
+                # dissimilarities are maximal for all model/real trace pairs
+                # thus, precision and fitness is minimal
+                [1.0, 1.0],
+                [1.0, 1.0],
+            ]
+        )
+    )
     assert matrix.calc_precision() == pytest.approx(0.0)
     assert matrix.calc_fitness() == pytest.approx(0.0)
 
 
 def test_calc_fitness_precision_2():
-    matrix = cf.DissimilarityMatrix(np.asarray([
-        # dissimilarities are minimal for all model/real trace pairs
-        # thus, precision and fitness is maximal (perfect match)
-        [0.0, 0.0],
-        [0.0, 0.0],
-    ]))
+    matrix = cf.DissimilarityMatrix(
+        np.asarray(
+            [
+                # dissimilarities are minimal for all model/real trace pairs
+                # thus, precision and fitness is maximal (perfect match)
+                [0.0, 0.0],
+                [0.0, 0.0],
+            ]
+        )
+    )
     assert matrix.calc_precision() == pytest.approx(1.0)
     assert matrix.calc_fitness() == pytest.approx(1.0)
 
 
 def test_calc_fitness_precision_3():
-    matrix = cf.DissimilarityMatrix(np.asarray([
-        # each real trace (column) can be represented by the model (traces in rows),
-        # but the model allows for extra traces not covered by the real log
-        # thus, fitness is maximal but precision is low
-        [0.0, 0.0],
-        [1.0, 1.0],
-    ]))
+    matrix = cf.DissimilarityMatrix(
+        np.asarray(
+            [
+                # each real trace (column) can be represented by the model (rows),
+                # but the model allows for extra traces not covered by the real log
+                # thus, fitness is maximal but precision is low
+                [0.0, 0.0],
+                [1.0, 1.0],
+            ]
+        )
+    )
     assert matrix.calc_precision() == pytest.approx(0.5)
     assert matrix.calc_fitness() == pytest.approx(1.0)
 
 
 def test_calc_fitness_precision_4():
-    matrix = cf.DissimilarityMatrix(np.asarray([
-        # each model trace (row) is equal to a real trace (column)
-        # but the model does not cover all real traces
-        # thus, precision is maximal but fitness is low
-        [1.0, 0.0],
-        [1.0, 0.0],
-    ]))
+    matrix = cf.DissimilarityMatrix(
+        np.asarray(
+            [
+                # each model trace (row) is equal to a real trace (column)
+                # but the model does not cover all real traces
+                # thus, precision is maximal but fitness is low
+                [1.0, 0.0],
+                [1.0, 0.0],
+            ]
+        )
+    )
     assert matrix.calc_precision() == pytest.approx(1.0)
     assert matrix.calc_fitness() == pytest.approx(0.5)
 
@@ -53,10 +69,14 @@ def test_calc_fitness_precision_4():
 def test_calc_fitness_precision_5():
     # this example just checks for floating point semantics
     # all dissimilarities are high, thus precision and fitness is low
-    matrix = cf.DissimilarityMatrix(np.asarray([
-        [1 - 1e-9, 1 - 1e-9],
-        [1 - 1e-9, 1 - 1e-9],
-    ]))
+    matrix = cf.DissimilarityMatrix(
+        np.asarray(
+            [
+                [1 - 1e-9, 1 - 1e-9],
+                [1 - 1e-9, 1 - 1e-9],
+            ]
+        )
+    )
     assert matrix.calc_precision() == pytest.approx(1e-9)
     assert matrix.calc_fitness() == pytest.approx(1e-9)
 
